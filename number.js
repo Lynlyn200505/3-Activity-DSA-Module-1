916a6c5180327f58ff8787c79d3f0b29a33b1b07
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-09-11 09:48:28 +0800

    number.js

---
+++ number.js
@@ -0,0 +1,20 @@
+const firstFavNum = 3;
+const secondFavNum = 17;
+//Addition
+const sum = firstFavNum + secondFavNum;
+console.log(sum);
+//Subtraction
+const difference = firstFavNum - secondFavNum;
+console.log(diffirence);
+//Multiplication
+const product = firstFavNum * secondFavNum;
+console.log(product);
+//Division
+const quotient = firstFavNum / secondFavNum;
+console.log(quotient);
+//Modulus
+const remainder = firstFavNum % secondFavNum;
+console.log(remainder);
+//Exponentiation(Power)
+const power = firstFavNum ** secondFavNum;
+console.log(power);
